d4781edb7da323245fa638fd2157371decf34ea7
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-05-01 17:21:25 -0700

    initial work on marketplace screen

--- components/landing/SignedIn.tsx
+++ components/landing/SignedIn.tsx
@@ -4,11 +4,16 @@ export const SignedInScreen = (props: { session: Session }) => {
   return (
     <div className="pr-8 pl-8">
       <h1 className="font-bold text-5xl mt-32">
-        Welcome back, <span className="text-primary">{props.session?.user?.name?.split(" ")[0]}!</span>
+        Welcome back,{" "}
+        <span className="text-primary">
+          {props.session?.user?.name?.split(" ")[0]}!
+        </span>
       </h1>
 
       <p className="text-lightGray font-bold text-md mt-16">FAVOURITED TEAMS</p>
-      <span className="text-lightGray text-sm font-medium">Looks like you have no favourited teams.</span>
+      <span className="text-lightGray text-sm font-medium">
+        Looks like you have no favourited teams.
+      </span>
     </div>
   );
 };
--- pages/marketplace.tsx
+++ pages/marketplace.tsx
@@ -1,23 +1,54 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Navbar } from "@/components/Navbar";
-import Head from 'next/head'
+import { MarketplaceScreen } from "@/components/screens/MarketplaceScreen";
+import { db } from "@/lib/db";
+import Head from "next/head";
 
-export default function MarketplacePage() {
-  return (
-    <>
-      <Head>
-        <title>Marketplace | Scout Machine</title>
-      </Head>
+export default function MarketplacePage({
+  posts,
+}: {
+  posts: (any & {
+    author: {
+      name: string;
+      image: string;
+    };
+  })[];
+}) {
+  {
+    return (
+      <>
+        <Head>
+          <title>Marketplace | Scout Machine</title>
+        </Head>
 
-      <Navbar active="Marketplace" />
+        <Navbar active="Marketplace" />
 
-      <Header
-        title="Marketplace"
-        desc="Have FRC parts you don't use anymore? Looking to sell or buy?"
-      />
+        <Header
+          title="Marketplace"
+          desc="Have FRC parts you don't use anymore? Looking to sell or buy?"
+        />
 
-      <Footer />
-    </>
-  );
+        <MarketplaceScreen  />
+
+        <Footer />
+      </>
+    );
+  }
 }
+
+export const getServerSideProps = async () => {
+  const posts = await db.post.findMany({
+    where: {},
+    include: {
+      author: {
+        select: {
+          image: true,
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
+  return { props: { posts: JSON.parse(JSON.stringify(posts)) } };
+};
