8c1a5f4999d62c493de910cbab3401ae486b1059
heybereket <semagnbereket@gmail.com>
2023-05-05 17:52:58 -0400

    change footer creds

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,17 @@
+import Link from "next/link";
+
 export const Footer = () => {
   return (
     <div className="px-4 py-2 pb-12 mt-10 rounded-lg flex flex-col items-center justify-center text-center">
       <div className="text-lightGray text-sm uppercase mb-3">
         Copyright © {new Date().getFullYear()} ⎯{" "}
-        <a href="https://frc6070.ca" target="_blank">
-          <span className="text-primary hover:text-white font-bold">
-            Team 6070 Gryphon Machine
-          </span>
-        </a>
+        <Link href="/" legacyBehavior>
+          <a>
+            <span className="text-primary hover:text-white font-bold">
+              Scout Machine
+            </span>
+          </a>
+        </Link>
       </div>
     </div>
   );
