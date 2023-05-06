f102bc4570fb8b2a22db0c725d1b34f832c52f53
heybereket <semagnbereket@gmail.com>
2023-05-06 16:35:46 -0400

    final import fix

--- pages/marketplace.tsx
+++ pages/marketplace.tsx
@@ -2,7 +2,7 @@ import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Navbar } from "@/components/navbar";
 import { MarketplaceScreen } from "@/components/screens/MarketplaceScreen";
-import { db } from "@/lib/db";
+import db from "@/lib/db";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 
