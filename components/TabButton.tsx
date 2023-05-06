0e3f5cf7fd51adfa8b93b7346e705cdc5ebf7e2c
heybereket <semagnbereket@gmail.com>
2023-05-06 13:49:24 -0400

    new tab button design

--- components/TabButton.tsx
+++ components/TabButton.tsx
@@ -12,11 +12,11 @@ export const TabButton = (props: Props) => {
   return (
     <button
       onClick={props.onClick}
-      className={`${props.className} px-5 py-2 rounded-lg font-semibold ${
+      className={`button ${
         props.active === props.tab
-          ? "bg-card border border-[#2A2A2A] text-white"
-          : "text-lightGray bg-card hover:border hover:border-[#2A2A2A] hover:text-white"
-      }`}
+          ? "text-white"
+          : "text-lightGray hover:text-white"
+      } bg-card font-semibold px-5 py-2 rounded-lg cursor-pointer select-none active:translate-y-2  active:[box-shadow:0_0px_0_0_#19999,0_0px_0_0_#19999] active:border-b-[0px] transition-all duration-150 [box-shadow:0_10px_0_0_#19999,0_15px_0_0_#19999] border-b-[5px] border-[#2A2A2A]`}
     >
       {props.children}
     </button>
