8c1cb9fe4eee198e68cd914093c84375adf297dd
heybereket <semagnbereket@gmail.com>
2023-05-08 00:00:46 -0400

    better loading indication

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,9 +1,10 @@
 export const Loading = () => {
   return (
-    <div className="flex items-center justify-center absolute bg-[#111111] inset-0 z-50">
+    <div className="flex flex-col items-center justify-center absolute bg-[#111111] inset-0 z-50">
       <h1 className="text-white font-black text-3xl animate-pulse">
         scout machine
       </h1>
+      <p className="text-xs text-lightGray mt-1">processing...</p>
     </div>
   );
 };
