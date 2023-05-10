636e44c96573237a50b5d1a376606a823d534a4b
heybereket <semagnbereket@gmail.com>
2023-05-10 16:46:23 -0400

    500 page

---
+++ pages/500.tsx
@@ -0,0 +1,20 @@
+import { Navbar } from "@/components/navbar";
+import Head from "next/head";
+
+export default function Page500() {
+  return (
+    <>
+      <Head>
+        <title>500 - Server Error | Scout Machine</title>
+      </Head>
+      <Navbar />
+      <div className="flex items-center justify-center flex-col mt-16">
+        <h1 className="text-5xl text-primary font-black">500 - Server Error</h1>
+        <p className="text-lightGray">
+          Uh oh, looks like there&apos;s a server issue. We&apos;ll be back
+          online soon - hang tight!
+        </p>
+      </div>
+    </>
+  );
+}
