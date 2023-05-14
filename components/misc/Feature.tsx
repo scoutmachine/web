f01dd13b9c2cda84b811d1acc7ae07a3cbebab3c
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-05-14 13:21:38 -0700

    added interaction for feature panels links

--- components/misc/Feature.tsx
+++ components/misc/Feature.tsx
@@ -1,6 +1,10 @@
+import { useRouter } from "next/router";
+
 export const Feature = (props: any) => {
+  const router = useRouter();
+  
   return (
-    <div className="rounded-full bg-card hover:border-gray-600 select-none cursor-auto text-sm text-lightGray hover:text-white transition-all duration-150 py-2 px-4 border border-[#2A2A2A] text-center flex items-center justify-center">
+    <div onClick={() => router.push(props.href)} className={`rounded-full bg-card hover:border-gray-600 select-none cursor-auto text-sm text-lightGray hover:text-white transition-all duration-150 py-2 px-4 border border-[#2A2A2A] text-center flex items-center justify-center ${props.href ? "cursor-pointer" : ""}`}>
       {props.name}
     </div>
   );
