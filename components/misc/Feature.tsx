efea673ecd9cb5ec66683446dfdc1cea896a310a
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-05-14 13:26:31 -0700

    strongly type feature component

--- components/misc/Feature.tsx
+++ components/misc/Feature.tsx
@@ -1,10 +1,16 @@
 import { useRouter } from "next/router";
+import { FC } from "react";
 
-export const Feature = (props: any) => {
+interface FeatureProps {
+  name: string;
+  href?: string;  
+}
+
+export const Feature: FC<FeatureProps> = (props) => {
   const router = useRouter();
-  
+
   return (
-    <div onClick={() => router.push(props.href)} className={`rounded-full bg-card hover:border-gray-600 select-none cursor-auto text-sm text-lightGray hover:text-white transition-all duration-150 py-2 px-4 border border-[#2A2A2A] text-center flex items-center justify-center ${props.href ? "cursor-pointer" : ""}`}>
+    <div onClick={() => router.push(props.href as string)} className={`rounded-full bg-card hover:border-gray-600 select-none cursor-auto text-sm text-lightGray hover:text-white transition-all duration-150 py-2 px-4 border border-[#2A2A2A] text-center flex items-center justify-center ${props.href ? "cursor-pointer" : ""}`}>
       {props.name}
     </div>
   );
