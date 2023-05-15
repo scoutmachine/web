c6ad967046be47ffd7806c957e8d15665fe9d5e0
heybereket <semagnbereket@gmail.com>
2023-05-15 18:47:28 -0400

    regex check for EPA

--- pages/api/match/epa.ts
+++ pages/api/match/epa.ts
@@ -6,13 +6,26 @@ export default async function getMatchEPA(
 ) {
   const { match } = req.query;
 
-  if (!match) res.status(400).send("No match code provided");
+  if (!match) {
+    res.status(400).send("No match code provided");
+    return;
+  }
 
-  const data = await fetch(`https://api.statbotics.io/v2/match/${match}`).then(
-    (res) => res.json()
-  );
-  res.status(200).send({
-    redEPA: data.red_epa_sum,
-    blueEPA: data.blue_epa_sum,
-  });
+  const matchCodeRegex = /^[A-Za-z0-9_-]+$/;
+  if (!matchCodeRegex.test(String(match))) {
+    res.status(400).send("Invalid match code");
+    return;
+  }
+
+  try {
+    const data = await fetch(
+      `https://api.statbotics.io/v2/match/${match}`
+    ).then((res) => res.json());
+    res.status(200).send({
+      redEPA: data.red_epa_sum,
+      blueEPA: data.blue_epa_sum,
+    });
+  } catch (error) {
+    res.status(500).send("An error occurred");
+  }
 }
