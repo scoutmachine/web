51671077548d594908635ce39bc363a4ebf29b92
warren yun <wyun2006@gmail.com>
2023-05-17 08:18:35 -0400

    clean up a bit more

--- pages/api/team/avatar.ts
+++ pages/api/team/avatar.ts
@@ -2,28 +2,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { fetchFIRST } from "@/lib/fetchFIRST";
 
 export const getTeamAvatar = async (req: NextApiRequest) => {
-  try {
-    const { team } = req.query;
-    return await fetchFIRST(`/avatars?teamNumber=${team}`);
-  } catch (e) {
-    return null;
-  }
+  const { team } = req.query;
+  return await fetchFIRST(`/avatars?teamNumber=${team}`);
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const avatar = await getTeamAvatar(req);
-  if (avatar !== null) {
-    if (avatar.status < 200 || avatar!.status >= 300) {
-      return res.status(avatar!.status).send("API request failed");
-    }
-
-    res.status(200).json({
-      avatar:
-        avatar.data.teams.length > 0
-          ? avatar.data.teams[0].encodedAvatar
-          : null,
-    });
-  } else {
-    res.status(500).json({ avatar: null });
+  const avatar = await getTeamAvatar(req).catch(() => {
+    res.status(500).json({ avatar: null })
+  });
+  if (avatar!.status < 200 || avatar!.status >= 300) {
+    return res.status(avatar!.status).send("API request failed");
   }
+
+  res.status(200).json({
+    avatar:
+      avatar!.data.teams.length > 0
+        ? avatar!.data.teams[0].encodedAvatar
+        : null,
+  });
 };
