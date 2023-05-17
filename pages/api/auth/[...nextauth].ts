213c45d63fdd9fc8ad7fb51a3bb5ee507dd96144
Autumn <62668093+Autumn-Ou@users.noreply.github.com>
2023-05-16 23:03:39 -0400

    longer

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -40,7 +40,7 @@ export const authOptions: AuthOptions = {
 
       if (userExists) {
         username = `${user.user.name?.replace(/\s/g, "").toLowerCase()}_${(
-            window.crypto.getRandomValues(new Uint32Array(1))[0]
+            window.crypto.getRandomValues(new Uint32Array(4))[0]
         )
           .toString(36)
           .substring(7)}`;
