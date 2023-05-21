52900dbec73b2816b263fd674673a3f983563b20
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-05-20 19:51:55 -0700

    hardcode contributers

--- components/screens/landing/SignedOut.tsx
+++ components/screens/landing/SignedOut.tsx
@@ -54,7 +54,32 @@ const IssueTemplate = (props: any) => {
 };
 
 export const SignedOutScreen = () => {
-  const [contributors, setContributors] = useState([]);
+  const [contributors, setContributors] = useState([
+    {
+      id: 68391329,
+      html_url: "https://github.com/heybereket",
+      avatar_url: "https://avatars.githubusercontent.com/u/68391329?v=4",
+      login: "heybereket",
+    },
+    {
+      id: 68178572,
+      html_url: "https://github.com/eternalmoon1234",
+      avatar_url: "https://avatars.githubusercontent.com/u/68178572?v=4",
+      login: "eternalmoon1234",
+    },
+    {
+      id: 62668093,
+      html_url: "https://github.com/Autumn-Ou",
+      avatar_url: "https://avatars.githubusercontent.com/u/62668093?v=4",
+      login: "Autumn-Ou",
+    },
+    {
+      id: 64022614,
+      html_url: "https://github.com/NebuDev14",
+      avatar_url: "https://avatars.githubusercontent.com/u/64022614?v=4",
+      login: "NebuDev14",
+    },
+  ]);
 
   // useEffect((): void => {
   //   const fetchContributors = async (): Promise<any> => {
