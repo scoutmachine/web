830bd1fa839e72e02ca50d9647cfa1888be5ccb0
Adam Garcia <adam@wematch.live>
2023-05-22 17:34:50 -0400

    Lint

--- components/screens/EventsScreen.tsx
+++ components/screens/EventsScreen.tsx
@@ -353,7 +353,7 @@ export const EventsScreen = (props: any): JSX.Element => {
         <button
           className="mt-5 bg-card hover:bg-[#191919] px-3 py-1 text-lightGray text-sm rounded-lg border border-[#2A2A2A] hover:text-black dark:text-white transition-all duration-150"
           onClick={(): void => {
-						fetch('/api/events/refresh')
+            fetch("/api/events/refresh");
           }}
         >
           <FaFileCsv className="mr-1 inline-block text-xs mb-[3px]" /> Refresh
--- pages/api/events/refresh.ts
+++ pages/api/events/refresh.ts
@@ -8,30 +8,29 @@ export default async function getEventRankings(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-	try {
-		console.log('Refreshing events')
-		const { data } = await tbaAxios.get<Event[]>(`events/${2023}`)
-		console.log(`Got ${data.length} events from TBA`)
-		
-		
-		// TODO: Add models in later
-		const formattedData = data.map((event) => {
-			return {
-				...event,
-				webcasts: undefined,
-				district: undefined,
-			}
-		})
-		
-		const response = await primsa.event.createMany({
-			data: formattedData,
-			skipDuplicates: true,
-		})
-	
-		return res.send({
-			message: `${response.count} events were created`,
-		})
-	} catch (error) {
-		return res.status(500).json({ error: error?.message ?? error });
-	}
+  try {
+    console.log("Refreshing events");
+    const { data } = await tbaAxios.get<Event[]>(`events/${2023}`);
+    console.log(`Got ${data.length} events from TBA`);
+
+    // TODO: Add models in later
+    const formattedData = data.map((event) => {
+      return {
+        ...event,
+        webcasts: undefined,
+        district: undefined,
+      };
+    });
+
+    const response = await primsa.event.createMany({
+      data: formattedData,
+      skipDuplicates: true,
+    });
+
+    return res.send({
+      message: `${response.count} events were created`,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error?.message ?? error });
+  }
 }
