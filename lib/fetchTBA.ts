544aebbc99515f587a554618cb3008832f6abbb5
Adam Garcia <adam@wematch.live>
2023-05-24 12:01:51 -0400

    Applied linting

--- lib/fetchTBA.ts
+++ lib/fetchTBA.ts
@@ -25,59 +25,59 @@ export const fetchTBA = async (
 };
 
 export type TBATeam = {
-	key: string
-	team_number: number
-	nickname?: string
-	name: string
-	city?: string
-	state_prov?: string
-	country?: string
-	address?: string
-	postal_code?: string
-	gmaps_place_id?: string
-	gmaps_url?: string
-	lat?: number
-	lng?: number
-	location_name?: string
-	website?: string
-	rookie_year?: number
-	motto?: string
-}
+  key: string;
+  team_number: number;
+  nickname?: string;
+  name: string;
+  city?: string;
+  state_prov?: string;
+  country?: string;
+  address?: string;
+  postal_code?: string;
+  gmaps_place_id?: string;
+  gmaps_url?: string;
+  lat?: number;
+  lng?: number;
+  location_name?: string;
+  website?: string;
+  rookie_year?: number;
+  motto?: string;
+};
 
 export type TBAEvent = {
-	key: string
-	name: string
-	event_code: string
-	event_type: number
-	// district?: 
-	city?: string
-	state_prov?: string
-	country?: string
-	start_date: string
-	end_date: string
-	year: number
-	short_name?: string
-	event_type_string: string
-	week?: number
-	address?: string
-	postal_code?: string
-	gmaps_place_id?: string
-	gmaps_url?: string
-	lat?: number
-	lng?: number
-	location_name?: string
-	timezone?: string
-	website?: string
-	first_event_id?: string
-	first_event_code?: string
-	webcasts?: {
-		channel: string
-		type: string
-		date?: string
-		file?: string
-	}[]
-	divison_keys?: string[]
-	parent_event_key?: string
-	playoff_type?: number
-	playoff_type_string?: string
-}
+  key: string;
+  name: string;
+  event_code: string;
+  event_type: number;
+  // district?:
+  city?: string;
+  state_prov?: string;
+  country?: string;
+  start_date: string;
+  end_date: string;
+  year: number;
+  short_name?: string;
+  event_type_string: string;
+  week?: number;
+  address?: string;
+  postal_code?: string;
+  gmaps_place_id?: string;
+  gmaps_url?: string;
+  lat?: number;
+  lng?: number;
+  location_name?: string;
+  timezone?: string;
+  website?: string;
+  first_event_id?: string;
+  first_event_code?: string;
+  webcasts?: {
+    channel: string;
+    type: string;
+    date?: string;
+    file?: string;
+  }[];
+  divison_keys?: string[];
+  parent_event_key?: string;
+  playoff_type?: number;
+  playoff_type_string?: string;
+};
