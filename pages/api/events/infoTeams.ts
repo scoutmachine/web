800ab0e9940b3c9189478946ca285e0b4df20a79
Adam Garcia <adam@wematch.live>
2023-05-24 16:41:24 -0400

    Event improvement

--- pages/api/events/infoTeams.ts
+++ pages/api/events/infoTeams.ts
@@ -1,15 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { fetchTBA } from "@/lib/fetchTBA";
 import { AxiosResponse } from "axios";
+import db from '@/lib/db'
 
 export default async function getEventTeams(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
   const { event } = req.query;
-  const data: void | AxiosResponse<any, any> = await fetchTBA(
-    `event/${event}/teams`
-  );
+	 
+	const rest = await db.team.findMany({
+		where: {
+			events: {
+				some: {
+					key: event as string
+				}
+			}
+		}
+	})
 
-  res.status(200).send(data);
+  res.status(200).send(rest);
 }
--- pages/api/v2/event/matches.ts
+++ pages/api/v2/event/matches.ts
@@ -2,48 +2,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { fetchTBA } from "@/lib/fetchTBA";
 import db from "@/lib/db";
 
-export default async function getEventRankings(
+export default async function getMatches(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
   try {
     const { event } = req.query;
 
-    const matches: any = await fetchTBA(`event/${event}/matches`);
     const eventMatches = await db.match.findMany({
       where: {
         event_key: String(event),
       },
     });
 
-    if (eventMatches.length > 0) {
-      res.status(200).send(eventMatches);
-    } else {
-      const createdMatches = await Promise.allSettled(
-        matches.map(async (match: any) => {
-          try {
-            const createdMatch = await db.match.upsert({
-              where: {
-                key: match.key,
-              },
-              create: {
-                ...match,
-              },
-              update: {},
-            });
-            return { status: "fulfilled", value: createdMatch };
-          } catch (error) {
-            return { status: "rejected", reason: error };
-          }
-        })
-      );
-
-      const successfulMatches = createdMatches
-        .filter((result) => result.status === "fulfilled")
-        .map((result) => (result as PromiseFulfilledResult<any>).value);
-
-      res.status(200).send((successfulMatches as any).value);
-    }
+		return res.status(200).send(eventMatches);
   } catch {
     res.status(400).send("Error");
   }
