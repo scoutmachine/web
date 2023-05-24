4b4e84e739a97e16832279ee1696d25d513f0b17
Adam Garcia <adam@wematch.live>
2023-05-24 12:00:09 -0400

    Finished seeding function for teams/events

--- pages/api/utils/seed.ts
+++ pages/api/utils/seed.ts
@@ -1,15 +1,15 @@
 import { tbaAxios, TBAEvent, TBATeam } from '@/lib/fetchTBA'
 import { RouterBuilder } from 'next-api-handler'
 import db from '@/lib/db'
+import chunk from 'lodash/chunk' 
 import { Team, Event, Prisma } from '@prisma/client'
 
 const router = new RouterBuilder()
 
 
-
 const handleTeamsETL = async () => {
 	console.log('Starting Teams ETL')
-	
+
 	let numTeamsInserted = 0
 	let pageNum = 0
 	while (true) {
@@ -65,44 +65,44 @@ const handleEventsETL = async () => {
 	let numEventsInserted = 0
 
 	// "address": "700 Monroe St SW, Huntsville, AL 35801, USA",
-  //   "city": "Huntsville",
-  //   "country": "USA",
-  //   "district": null,
-  //   "division_keys": [],
-  //   "end_date": "2023-04-08",
-  //   "event_code": "alhu",
-  //   "event_type": 0,
-  //   "event_type_string": "Regional",
-  //   "first_event_code": "alhu",
-  //   "first_event_id": null,
-  //   "gmaps_place_id": "ChIJBwUw_FdrYogRMsP0V0W5Zqk",
-  //   "gmaps_url": "https://maps.google.com/?q=700+Monroe+St+SW,+Huntsville,+AL+35801,+USA&ftid=0x88626b57fc300507:0xa966b94557f4c332",
-  //   "key": "2023alhu",
-  //   "lat": 34.72671,
-  //   "lng": -86.5903795,
-  //   "location_name": "700 Monroe St SW",
-  //   "name": "Rocket City Regional",
-  //   "parent_event_key": null,
-  //   "playoff_type": 10,
-  //   "playoff_type_string": "Double Elimination Bracket (8 Alliances)",
-  //   "postal_code": "35801",
-  //   "short_name": "Rocket City",
-  //   "start_date": "2023-04-05",
-  //   "state_prov": "AL",
-  //   "timezone": "America/Chicago",
-  //   "webcasts": [
-  //     {
-  //       "channel": "firstinspires13",
-  //       "type": "twitch"
-  //     },
-  //     {
-  //       "channel": "firstinspires14",
-  //       "type": "twitch"
-  //     }
-  //   ],
-  //   "website": "http://firstinalabama.org/events/frc-events/",
-  //   "week": 5,
-  //   "year": 2023
+	//   "city": "Huntsville",
+	//   "country": "USA",
+	//   "district": null,
+	//   "division_keys": [],
+	//   "end_date": "2023-04-08",
+	//   "event_code": "alhu",
+	//   "event_type": 0,
+	//   "event_type_string": "Regional",
+	//   "first_event_code": "alhu",
+	//   "first_event_id": null,
+	//   "gmaps_place_id": "ChIJBwUw_FdrYogRMsP0V0W5Zqk",
+	//   "gmaps_url": "https://maps.google.com/?q=700+Monroe+St+SW,+Huntsville,+AL+35801,+USA&ftid=0x88626b57fc300507:0xa966b94557f4c332",
+	//   "key": "2023alhu",
+	//   "lat": 34.72671,
+	//   "lng": -86.5903795,
+	//   "location_name": "700 Monroe St SW",
+	//   "name": "Rocket City Regional",
+	//   "parent_event_key": null,
+	//   "playoff_type": 10,
+	//   "playoff_type_string": "Double Elimination Bracket (8 Alliances)",
+	//   "postal_code": "35801",
+	//   "short_name": "Rocket City",
+	//   "start_date": "2023-04-05",
+	//   "state_prov": "AL",
+	//   "timezone": "America/Chicago",
+	//   "webcasts": [
+	//     {
+	//       "channel": "firstinspires13",
+	//       "type": "twitch"
+	//     },
+	//     {
+	//       "channel": "firstinspires14",
+	//       "type": "twitch"
+	//     }
+	//   ],
+	//   "website": "http://firstinalabama.org/events/frc-events/",
+	//   "week": 5,
+	//   "year": 2023
 	for (let year = startYear; year <= endYear; year++) {
 		console.log(`Parsing Events for ${year}`)
 		const eventsRequest = await tbaAxios.get<TBAEvent[]>(`/events/${year}`)
@@ -164,43 +164,60 @@ const addTeamsToEvents = async () => {
 		}
 	})
 
-	for (const event of allEvents) {
-		console.log('Adding Teams to Event: ', event.key)
-		// const participatingTeams = await tbaAxios.get(`/event/${event.key}/teams/keys`)
-		
-		const abc = await db.event.update({
-			where: {
-				key: event.key
-			},
-			data: {
-				teams: {
-					connect: {
-						team_number: 4
-					}
-				}
-			}
-		})
 
-		console.log('abc: ', abc)
+	const eventPromiseArr = allEvents.map(event => async () => {
+		 try {
+			 const participatingTeams = await tbaAxios.get(`/event/${event.key}/teams/keys`)
+	 
+			 await db.event.update({
+				 where: {
+					 key: event.key
+				 },
+				 data: {
+					 teams: {
+						 connect: participatingTeams.data.map((teamKey: string) => {
+							 return {
+								 team_number: parseInt(teamKey.substring(3))
+							 }
+						 })
+					 }
+				 }
+			 })
 
-		break
+			 return event.key
+		 } catch (error) {
+			 console.log('Error adding teams to event: ', event.key)
+			 console.log(error)
+			 return undefined
+		 }
+	})
+
+	// Chunk the promises to avoid overloading the DB
+	const chunkedPromises = chunk(eventPromiseArr, 50)
+	console.log(`Splitting Events into ${chunkedPromises.length} chunks`)
+
+	// loop over chunkedPromises with index
+	for (const [index, eventPromise] of chunkedPromises.entries()) {
+		const allEvents = await Promise.all(eventPromise.map((fn: Function) => fn()))
+		console.log(`Event Chunk: ${index + 1}/${chunkedPromises.length}. Added the following events: ${allEvents.filter(event => event !== undefined).join(', ')}`)
 	}
-	
-	
 
+	return true
 }
 
 router.post(async (req, res) => {
 	// First download all teams
 	try {
-		// const teamsInserted = await handleTeamsETL()
-		// const eventsInserted = await handleEventsETL()
-
+		const teamsInserted = await handleTeamsETL()
+		const eventsInserted = await handleEventsETL()
 		await addTeamsToEvents()
-		res.status(200).json({ 
+
+		res.status(200).json({
 			message: 'Success',
+			teamsInserted,
+			eventsInserted,
 			// teamsInserted
-		})	
+		})
 
 	} catch (error) {
 		console.log('error: ', error)
