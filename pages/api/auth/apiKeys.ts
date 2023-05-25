b401ba5ab768c6bec8273dc406faeca0db8ac139
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-05-25 13:57:21 -0700

    add route to create a new api key

---
+++ pages/api/auth/apiKeys.ts
@@ -0,0 +1,31 @@
+import db from "@/lib/db";
+import { generateRandomString } from "@/utils/generateRandomString";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession, Session } from "next-auth";
+import { authOptions } from "./[...nextauth]";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session: Session = (await getServerSession(
+    req,
+    res,
+    authOptions
+  )) as Session;
+
+  if (!session) res.status(400).send("You are not logged in!");
+
+  if (req.method === "POST") {
+    const key = generateRandomString(32);
+    const apiKey = await db.apiKey.create({
+    data: {
+      key: key,
+      // @ts-ignore
+      userId: session.user.id
+    }
+    });
+
+    return res.status(200).json({ apiKey: apiKey.key });
+  }
+}
