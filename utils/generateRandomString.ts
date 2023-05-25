df11005ff6742dae5337c0f1966bb3f01f74c4ff
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-05-25 13:56:30 -0700

    add generate random string util func

---
+++ utils/generateRandomString.ts
@@ -0,0 +1,12 @@
+export function generateRandomString(length: number): string {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomString = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+
+  return randomString;
+}
