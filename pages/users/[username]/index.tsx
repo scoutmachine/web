686a2b640fdc853bdf351a9865f742c4d93ad77e
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-05-26 12:03:57 -0500

    lint

--- pages/users/[username]/index.tsx
+++ pages/users/[username]/index.tsx
@@ -51,7 +51,8 @@ export default function UserProfilePage({ user, apiKeys }: any) {
   };
 
   const handleCopyApiKey = (apiKey: string) => {
-    navigator.clipboard.writeText(apiKey)
+    navigator.clipboard
+      .writeText(apiKey)
       .then(() => {
         notify("API key copied to clipboard!", <FaCheck />);
       })
@@ -126,21 +127,22 @@ export default function UserProfilePage({ user, apiKeys }: any) {
               <ul className="mt-4 space-y-2">
                 {userApiKeys.map((apiKey) => (
                   <li key={apiKey} className="flex items-center">
-                                      <code className="bg-gray-800 rounded-lg px-3 py-1 text-sm text-gray-300">
-                    {apiKey}
+                    <code className="bg-gray-800 rounded-lg px-3 py-1 text-sm text-gray-300">
+                      {apiKey}
+                      <button
+                        className="ml-2 bg-transparent border-none p-1 cursor-pointer"
+                        onClick={() => handleCopyApiKey(apiKey)}
+                      >
+                        <FaCopy />
+                      </button>
+                    </code>
                     <button
-                      className="ml-2 bg-transparent border-none p-1 cursor-pointer"
-                      onClick={() => handleCopyApiKey(apiKey)}
-                    >
-                      <FaCopy />
-                    </button>
-                  </code><button
                       className="ml-2 flex items-center bg-red-600 hover:bg-red-500 text-white text-sm font-medium rounded-md px-2 py-1 transition-colors duration-300 focus:outline-none"
                       onClick={() => handleDeleteApiKey(apiKey)}
                     >
                       Delete
                     </button>
-                    </li>
+                  </li>
                 ))}
               </ul>
             ) : (
