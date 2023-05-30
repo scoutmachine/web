44bcd10fe77d0293df6734ecdc1dea15e354b3df
heybereket <semagnbereket@gmail.com>
2023-05-30 07:45:30 -0400

    more meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,15 @@ export default function Document(): JSX.Element {
         <meta name="og:image" content="/ScoutMachineBanner.png" />
         <meta name="theme-color" content="#FBBB04" />
         <link rel="shortcut icon" href="/smLogo.png" />
+
+        <meta name="application-name" content="Scout Machine" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Scout Machine" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/smLogo180.png" />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
       </Head>
 
       <body className="bg-[#f7f7f7] dark:bg-[#141414]">
