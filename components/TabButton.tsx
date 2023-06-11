dbcabb866e94f649b543c885d081917bb0c4bfb6
heybereket <semagnbereket@gmail.com>
2023-06-11 09:55:25 -0400

    style change

--- components/TabButton.tsx
+++ components/TabButton.tsx
@@ -11,14 +11,16 @@ interface Props {
 export const TabButton = (props: Props) => {
   return (
     <div
-      className={`${props.className} bg-card hover:text-white transition-all duration-150 ${
+      className={`${
+        props.className
+      } bg-card hover:text-white transition-all duration-150 ${
         props.active === props.tab ? "text-white" : "text-lightGray"
       } font-semibold rounded-lg`}
     >
       <div className="bg-gradient-to-b from-[#191919] to-card drop-shadow-[0_0.125rem_0.125rem_rgba(0,0,0,0.25)] rounded-lg">
         <button
           onClick={props.onClick}
-          className={`bg-card px-5 py-2 ${props.className ?? 'rounded-lg'}`}
+          className={`bg-card px-5 py-2 ${props.className ?? "rounded-lg"}`}
         >
           {props.children}
         </button>
